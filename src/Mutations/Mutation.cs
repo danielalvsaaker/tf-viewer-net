3225cfa7bb3a3f59bf82869d923b92e192964209
Daniel Alvsåker <daniel.alvsaaker@protonmail.com>
2023-09-04 18:11:09 +0200

    Add Mutation type
    Former-commit-id: 69593d44f4d6b3591a0f52e4bb63a6de61ad2ec8

---
+++ src/Mutations/Mutation.cs
@@ -0,0 +1,26 @@
+﻿using HotChocolate.Types;
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mutations;
+
+public class Mutation
+{
+    [UseMutationConvention(PayloadFieldName = nameof(activityId))]
+    public async Task<string?> DeleteActivity(
+        string userId,
+        string activityId,
+        ApplicationDbContext context)
+    {
+        var rows = await context.Activities
+            .Where(activity => activity.UserId == userId)
+            .Where(activity => activity.ActivityId == activityId)
+            .ExecuteDeleteAsync();
+
+        return rows switch
+        {
+            > 0 => activityId,
+            _ => null,
+        };
+    }
+}
