114a2d11476ff31639ab4efaf619e3af5aefc6b1
Daniel Alvsåker <daniel.alvsaaker@protonmail.com>
2023-09-04 10:32:16 +0200

    Setup package with deps in flake
    Former-commit-id: 8124f3b0ca3cc1af21923149526821fa154aaf52

--- flake.nix
+++ flake.nix
@@ -1,21 +1,31 @@
 {
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
   outputs = { self, nixpkgs }:
   let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
     dotnet-sdk = pkgs.dotnet-sdk_7;
     dotnet-runtime = pkgs.dotnet-aspnetcore_7;
-  in {
-    devShells.x86_64-linux = {
-      default = pkgs.mkShell {
-        packages = with pkgs; [
-          dotnet-sdk
-          dotnet-runtime
-          omnisharp-roslyn
-        ];
+  in rec {
+    packages.x86_64-linux.default = pkgs.buildDotnetModule {
+      pname = "tf-viewer";
+      version = "0.1";
+      meta.mainProgram = "Server";
 
-        LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath [ pkgs.libspatialite pkgs.sqlite ];
-      };
+      src = ./.;
+      nugetDeps = ./deps.nix;
+      projectFile = "tf-viewer-net.sln";
+      dotnet-sdk = pkgs.dotnet-sdk_7;
+      dotnet-runtime = pkgs.dotnet-aspnetcore_7;
+
+      executables = [ "Server" ];
+      runtimeDeps = [ pkgs.libspatialite pkgs.sqlite ];
+    };
+
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      inputsFrom = [ packages.x86_64-linux.default ];
+
+      LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath [ pkgs.libspatialite pkgs.sqlite ];
     };
   };
 }
