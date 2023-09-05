4b1c4333cebb4f046c5a9c4267fe6a2c9b294201
Daniel Alvsåker <daniel.alvsaaker@protonmail.com>
2023-09-05 18:20:29 +0200

    Set picture to be mutable
    Former-commit-id: f74e56d892c363854d64b931ca27f1a062884676

--- src/Core/User.cs
+++ src/Core/User.cs
@@ -8,7 +8,7 @@ public class User
     public required string Id { get; init; }
     public required string Username { get; init; }
     public required string Name { get; init; }
-    public Uri? Picture { get; init; }
+    public Uri? Picture { get; set; }
 
     [GraphQLIgnore]
     public IEnumerable<Activity> Activities { get; set; } = new List<Activity>();
