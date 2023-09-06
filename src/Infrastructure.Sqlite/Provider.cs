f683c4ef51528a1d8c6f6c9eafa12fff72f4f7ad
danielalvsaaker <30574112+danielalvsaaker@users.noreply.github.com>
2023-09-06 09:06:01 +0200

    Migration assembly per provider (#2)
    * Set up migration assemblies per provider
    
    * Remove usage of literal assembly name
    
    * Update dependencies

---
+++ src/Infrastructure.Sqlite/Provider.cs
@@ -0,0 +1,7 @@
+namespace Infrastructure.Sqlite;
+
+public static class Provider
+{
+    public const string Name = "Sqlite";
+    public static readonly string Assembly = typeof(Provider).Assembly.FullName!;
+}
