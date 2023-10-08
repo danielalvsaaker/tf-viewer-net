884bcc004a32ad54f5931931066b277d68d5c571
Daniel Alvsåker <daniel.alvsaaker@protonmail.com>
2023-10-08 17:13:57 +0200

    Update to .NET 8 RC

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
   outputs = { self, nixpkgs }:
   let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    dotnet-sdk = pkgs.dotnet-sdk_7;
-    dotnet-runtime = pkgs.dotnet-aspnetcore_7;
+    dotnet-sdk = pkgs.dotnet-sdk_8;
+    dotnet-runtime = pkgs.dotnet-aspnetcore_8;
   in rec {
     packages.x86_64-linux.default = pkgs.buildDotnetModule {
       pname = "tf-viewer";
@@ -14,9 +14,9 @@
 
       src = ./src;
       nugetDeps = ./deps.nix;
-      projectFile = "tf-viewer-net.sln";
-      dotnet-sdk = pkgs.dotnet-sdk_7;
-      dotnet-runtime = pkgs.dotnet-aspnetcore_7;
+      projectFile = "Server/Server.csproj";
+
+      inherit dotnet-sdk dotnet-runtime;
 
       executables = [ "Server" ];
       runtimeDeps = [ pkgs.libspatialite pkgs.sqlite ];
