43b3b1983a01d95749d6fc054ac7fa8eae12a1b6
Daniel Alvsåker <daniel.alvsaaker@protonmail.com>
2023-10-21 16:08:28 +0200

    Set up build/release action

--- flake.nix
+++ flake.nix
@@ -4,26 +4,39 @@
   outputs = { self, nixpkgs }:
   let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    lib = pkgs.lib;
     dotnet-sdk = pkgs.dotnet-sdk_8;
     dotnet-runtime = pkgs.dotnet-aspnetcore_8;
   in rec {
-    packages.x86_64-linux.default = pkgs.buildDotnetModule {
-      pname = "tf-viewer";
-      version = "0.1";
-      meta.mainProgram = "Server";
+    packages.x86_64-linux = rec {
+      server = pkgs.buildDotnetModule {
+        pname = "tf-viewer";
+        version = "0.1";
+        meta.mainProgram = "Server";
 
-      src = ./src;
-      nugetDeps = ./deps.nix;
-      projectFile = "Server/Server.csproj";
+        src = ./src;
+        nugetDeps = ./deps.nix;
+        projectFile = "Server/Server.csproj";
 
-      inherit dotnet-sdk dotnet-runtime;
+        inherit dotnet-sdk dotnet-runtime;
 
-      executables = [ "Server" ];
-      runtimeDeps = [ pkgs.libspatialite pkgs.sqlite ];
+        executables = [ "Server" ];
+        runtimeDeps = [ pkgs.libspatialite pkgs.sqlite ];
+      };
+
+      image = pkgs.dockerTools.buildLayeredImage {
+        name = "tf-viewer";
+        tag = "latest";
+
+        config = {
+          WorkingDir = "/data";
+          Cmd = [ (lib.getExe server) ];
+        };
+      };
     };
 
     devShells.x86_64-linux.default = pkgs.mkShell {
-      inputsFrom = [ packages.x86_64-linux.default ];
+      inputsFrom = [ packages.x86_64-linux.server ];
 
       LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath [ pkgs.libspatialite pkgs.sqlite ];
     };
