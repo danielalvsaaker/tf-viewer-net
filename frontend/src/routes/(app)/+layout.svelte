4b9d96ab00c560ae988573d3303345d88660160f
danielalvsaaker <30574112+danielalvsaaker@users.noreply.github.com>
2023-11-15 12:23:58 +0100

    Create sign in page

---
+++ frontend/src/routes/(app)/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import UserNav from '$lib/components/UserNav.svelte';
+    import Nav from '$lib/components/Nav.svelte';
+    import { page } from '$app/stores';
+
+    const user = $page.data.session?.user;
+</script>
+
+<div class="flex h-16 items-center px-4">
+    <Nav user={user?.id} />
+    <div class="ml-auto flex items-center space-x-4">
+        {#if user}
+            <UserNav />
+        {/if}
+    </div>
+</div>
+<main class="container">
+    <slot />
+</main>
---
+++ frontend/src/routes/(auth)/auth/signIn/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import Button from '$lib/components/ui/button/button.svelte';
+    import { signIn } from '@auth/sveltekit/client';
+
+    export let data;
+</script>
+
+<div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+    <div class="space-y-2 text-center">
+        <h1 class="text-2xl font-semibold tracking-tight">Sign in</h1>
+    </div>
+    <Button
+        variant="outline"
+        type="button"
+        on:click={() => signIn(data.provider.id, { callbackUrl: '/', redirect: true })}
+    >
+        Sign in
+    </Button>
+</div>
