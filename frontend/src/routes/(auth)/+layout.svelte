fbeef507e0beef6949b961995d18d05da6f4a5d0
danielalvsaaker <30574112+danielalvsaaker@users.noreply.github.com>
2023-11-28 22:34:59 +0100

    Fix sign in page

--- frontend/src/routes/(auth)/+layout.svelte
+++ frontend/src/routes/(auth)/+layout.svelte
@@ -1,5 +1,3 @@
-<main
-    class="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-nonel lg:grid-cols-2 lg:px-0"
->
+<main class="container relative h-screen flex-col items-center justify-center grid">
     <slot />
 </main>
